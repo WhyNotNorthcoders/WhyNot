8172fce9c6d622cb89aca16802df394b93ef53b0
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2022-11-14 13:49:19 +0000

    header added

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import * as React from "react";
+import { View } from "react-native";
 import Tabbar from "./components/Tabbar";
+import Header from "./components/Header";
 
 export default function App() {
-  return <Tabbar />;
+  return (
+    <View>
+      <Header />
+      <Tabbar />
+    </View>
+  );
 }
