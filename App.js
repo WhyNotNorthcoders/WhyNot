15f58df531f084bf8c19f00e4b77cdbc28c5ca70
Michael Whitham <michaelawhitham@hotmail.co.uk>
2022-11-14 17:11:28 +0000

    create login screen

--- App.js
+++ App.js
@@ -1,6 +1,37 @@
 import * as React from "react";
-import Tabbar from "./components/Tabbar";
+import { useState } from "react";
+import LoginScreen from "./components/LoginScreen";
+import ActivePages from "./components/ActivePages";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { userContext } from "./context";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <Tabbar />;
+  const [LoggedIn, setLoggedIn] = useState(false);
+
+  const toggleLogIn = () => {
+    setLoggedIn(true);
+  };
+
+  return (
+    <userContext.Provider value={setLoggedIn}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {LoggedIn ? (
+            <Stack.Group>
+              <Stack.Screen name="Why Not?" component={ActivePages} />
+            </Stack.Group>
+          ) : (
+            <Stack.Group>
+              <Stack.Screen name="LoginScreen">
+                {(props) => <LoginScreen {...props} extraData={toggleLogIn} />}
+              </Stack.Screen>
+            </Stack.Group>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </userContext.Provider>
+  );
 }
---
+++ components/ActivePages.jsx
@@ -0,0 +1,11 @@
+import Tabbar from "./Tabbar";
+
+const ActivePages = () => {
+  return (
+    <>
+      <Tabbar />
+    </>
+  );
+};
+
+export default ActivePages;
