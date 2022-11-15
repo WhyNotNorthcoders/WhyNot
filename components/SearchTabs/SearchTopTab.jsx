230058421f3721c766b0f008f99136f01c96612a
Michael Whitham <michaelawhitham@hotmail.co.uk>
2022-11-15 16:50:51 +0000

    styling tabbar and adding to profile page

--- components/SearchTabs/SearchTopTab.jsx
+++ components/SearchTabs/SearchTopTab.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import SearchTop from "./SearchTop";
 import SearchUser from "./SearchUsers";
