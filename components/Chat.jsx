e27ef42973a4e123f2290d17c61b60403147a82a
Olumorothy <iseoluwa89@gmail.com>
2022-11-16 12:11:09 +0000

    added the chat functionality update

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,72 +1,87 @@
-import {GiftedChat} from "react-native-gifted-chat";
-import React, {useState, useEffect, useLayoutEffect,useCallback} from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+import React, {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useCallback,
+} from "react";
 
 import { TouchableOpacity, Text } from "react-native";
-import { addDoc, orderBy, query, onSnapshot, collection } from "firebase/firestore";
+import {
+  addDoc,
+  orderBy,
+  query,
+  onSnapshot,
+  collection,
+} from "firebase/firestore";
 import { signOut } from "firebase/auth";
-import { auth, database} from "../config/firebaseConfig"
-import {useNavigation} from '@react-navigation/native'
-import {AntDesign} from '@expo/vector-icons'
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { auth, database } from "../config/firebaseConfig";
+import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function Chat(){
-    const[messages, setMessages] = useState([])
-    // const navigation = useNavigation();
+export default function Chat() {
+  const [messages, setMessages] = useState([]);
+  //   const navigation = useNavigation();
 
-    // const onSignOut =()=>{
-    //     signOut(auth).catch((err)=> {
-    //         console.log(err)
-    //     })
-    // }
+  const onSignOut = () => {
+    signOut(auth).catch((err) => {
+      console.log(err);
+    });
+  };
 
-    // useLayoutEffect(()=>{
-    //     navigation.setOptions({
-    //         headerRight: () => (
-    //             <TouchableOpacity 
-    //             style={{marginRight: 10}}
-    //             onPress={onSignOut}>
-    //                 <AntDesign name="logout" size={24} color={'#C5C5C7'} style={{marginRight:10}}/>
-    //             </TouchableOpacity>
-    //         )
-    //     })
-    // }, [navigation]);
+  //   useLayoutEffect(() => {
+  //     navigation.setOptions({
+  //       headerRight: () => (
+  //         <TouchableOpacity style={{ marginRight: 10 }} onPress={onSignOut}>
+  //           <AntDesign
+  //             name="logout"
+  //             size={24}
+  //             color="#C5C5C7"
+  //             style={{ marginRight: 10 }}
+  //           />
+  //         </TouchableOpacity>
+  //       ),
+  //     });
+  //   }, [navigation]);
 
-    useLayoutEffect(()=>{
-        const collectionRef = collection(database, 'Chats')
-        const q = query(collectionRef, orderBy('createdAt', 'desc'));
-        const unsubscribe = onSnapshot(q, snapshot =>{
-            setMessages(
-                snapshot.docs.map(doc => ({
-                    _id: doc.id,
-                    createdAt: doc.data().createdAt.toDate(),
-                    text: doc.data().text,
-                    user: doc.data().user
-                }))
-            )
-        });
-        return () => unsubscribe();
-    }, []);
+  useLayoutEffect(() => {
+    const collectionRef = collection(database, "Chats");
+    const q = query(collectionRef, orderBy("createdAt", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setMessages(
+        snapshot.docs.map((doc) => ({
+          _id: doc.id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user,
+        }))
+      );
+    });
+    return () => unsubscribe();
+  }, []);
 
-    const onSend = useCallback((messages = [])=>{
-        setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
-        const {_id, createdAt, text, user} = messages[0];
-        addDoc(collection(database, 'Chats'),{
-            _id,
-            createdAt,
-            text,
-            user
-        });
-    }, [])
-    return (
-        <GiftedChat 
-        messages ={messages}
-        onSend={messages => onSend(messages)}
-        user={{
-            id: auth?.currentUser?.email
-        }}
-        messagesContainerStyle={{
-            backgroundColor: '#fff'
-
-        }}/>
-    )
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+    const { _id, createdAt, text, user } = messages[0];
+    addDoc(collection(database, "Chats"), {
+      _id,
+      createdAt,
+      text,
+      user,
+    });
+  }, []);
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        id: auth?.currentUser?.email,
+      }}
+      messagesContainerStyle={{
+        backgroundColor: "#fff",
+      }}
+    />
+  );
 }
