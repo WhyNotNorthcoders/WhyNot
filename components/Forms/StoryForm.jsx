0e68d9a02d23887975f62453562bc412328dd68c
GxrySu <garysu2013@gmail.com>
2022-11-16 14:08:30 +0000

    refactor styling on StoryForm

--- components/Forms/StoryForm.jsx
+++ components/Forms/StoryForm.jsx
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   Image,
   Pressable,
+  ScrollView,
 } from "react-native";
 import DropDownPicker from "react-native-dropdown-picker";
 import DatePicker from "react-native-modern-datepicker";
@@ -18,6 +19,7 @@ import { Rating, AirbnbRating } from "react-native-ratings";
 
 const StoryForm = () => {
   const [storyTitle, setStoryTitle] = useState("");
+  const [description, setDescription] = useState("");
   const [location, setLocation] = useState("");
   const [categoryOpen, setCategoryOpen] = useState(false);
   const [category, setCategory] = useState("");
@@ -52,17 +54,15 @@ const StoryForm = () => {
     }
   };
 
-  const finishRating = (rating) => {
-    setRating(rating);
-  };
-
-  console.log(category);
-
   return (
-    <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={{ textAlign: "center" }}>Complete Story Form</Text>
       <View>
-        <TextInput style={styles.textInput} placeholder="Enter Story Title" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter Story Title"
+          onChange={setStoryTitle}
+        />
         <View>
           <DropDownPicker
             style={{
@@ -79,6 +79,12 @@ const StoryForm = () => {
             setItems={setItems}
           />
         </View>
+        <TextInput
+          style={styles.description}
+          multiline={true}
+          placeholder="Enter Your Experience"
+          onChange={setDescription}
+        ></TextInput>
         <TextInput style={styles.textInput} placeholder="Enter Location" />
         <View>
           <TouchableOpacity onPress={toggleDate}>
@@ -116,7 +122,7 @@ const StoryForm = () => {
             {image && (
               <Image
                 source={{ uri: image }}
-                style={{ width: 200, height: 200 }}
+                style={{ width: "100%", height: "100%" }}
               />
             )}
           </View>
@@ -126,30 +132,47 @@ const StoryForm = () => {
         </View>
         {/* <AirbnbRating defaultRating={1} size={30} reviewSize={20}  reviewColor={"#CAD2C5"} reviews={["Rating:","Rating:","Rating:","Rating:","Rating:"]} onFinishRating={setRating} /> */}
         <View style={styles.ratingContainer}>
-        <Text>Rating: </Text>
+          <Text style={{ color: "#CAD2C5" }}>Rating: </Text>
           <Rating
+            style={styles.rating}
             type={"custom"}
-            tintColor={"#52796F"}
+            ratingColor={"#FFF36D"}
+            tintColor={"#354F52"}
+            ratingBackgroundColor={"#CAD2C5"}
             minValue={0}
             ratingCount={5}
-            onFinishRating={finishRating}
+            onFinishRating={(rating) => {
+              setRating(rating);
+            }}
           />
         </View>
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.bucketListButton}>
-          <Text style={{ textAlign: "center" }}>Complete Story</Text>
+        <TouchableOpacity style={styles.completeStoryButton}>
+          <Text style={{ textAlign: "center", color: "#CAD2C5" }}>
+            Complete Story
+          </Text>
         </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     margin: 5,
-    padding: 10,
+    padding: 25,
+    borderRadius: 25,
     backgroundColor: "#52796F",
+  },
+
+  description: {
+    color: "black",
+    backgroundColor: "#CAD2C5",
+    borderRadius: 6,
+    borderWidth: 1,
+    padding: 5,
+    margin: 6,
   },
 
   textInput: {
@@ -165,13 +188,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  bucketListButton: {
+
+  completeStoryButton: {
     justifyContent: "center",
     alignItems: "center",
     width: "50%",
+    bottom: 20,
     margin: 10,
-    paddingTop: 5,
-    paddingBottom: 5,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 4,
     borderWidth: 1,
     borderColor: "#2F3E46",
     backgroundColor: "#84A98C",
@@ -184,6 +210,7 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     elevation: 3,
     backgroundColor: "#84A98C",
+    marginTop: 5,
   },
   buttonText: {
     color: "#CAD2C5",
@@ -191,8 +218,22 @@ const styles = StyleSheet.create({
   imageContainer: {
     width: 202,
     height: 202,
-    borderWidth: 1,
-    borderColor: "black",
+    borderWidth: 2,
+    borderColor: "#2F3E46",
+  },
+
+  ratingContainer: {
+    justifyContent: "center",
+    alignitems: "center",
+    alignSelf: "center",
+    marginBottom: 15,
+    margin: 10,
+    borderRadius: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingRight: 40,
+    paddingLeft: 40,
+    backgroundColor: "#354F52",
   },
 });
 
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,7 +1,6 @@
 import { signOut } from "firebase/auth";
 import { Button, SafeAreaView, Text, View } from "react-native";
 import { auth, onAuthStateChanged } from "../config/firebaseConfig";
-import StoryForm from "../components/Forms/StoryForm";
 
 const Home = ({ navigation }) => {
   const handleLogout = () => {
@@ -13,9 +12,6 @@ const Home = ({ navigation }) => {
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Home page</Text>
         <Button title="Log out" onPress={handleLogout} />
-      </View>
-      <View>
-        <StoryForm />
       </View>
     </>
   );
