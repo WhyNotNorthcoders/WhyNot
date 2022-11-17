eeac1cbd9882221cf8eed93282a40e9e71c81491
GxrySu <garysu2013@gmail.com>
2022-11-17 12:41:42 +0000

    add EventCard and implement working pop up for each card

---
+++ components/SearchTabs/EventsCard.jsx
@@ -0,0 +1,132 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Linking,
+  Image,
+  Modal,
+  Pressable,
+} from "react-native";
+
+const EventsCard = ({ item }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        setModalVisible(true);
+      }}
+    >
+      <View style={styles.item}>
+        <Text>Title: {item.title}</Text>
+        <Image
+          style={{ height: 100, width: 100, borderRadius: 50 }}
+          source={{ uri: item.image }}
+        />
+        <Text>
+          Address: {item.address[0]}, {item.address[1]}
+        </Text>
+        <Text>{item.date.when}</Text>
+        <View style={styles.imageContainer}></View>
+        <Text
+          style={{ color: "blue" }}
+          onPress={() => {
+            Linking.openURL(item.link);
+          }}
+        >
+          More About This Event
+        </Text>
+      </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View>
+              <Text>Title: {item.title}</Text>
+              <Image
+                style={{ height: 100, width: 100, borderRadius: 50 }}
+                source={{ uri: item.image }}
+              />
+              <Text>Description: {item.description}</Text>
+              <Text>
+                Address: {item.address[0]}, {item.address[1]}
+              </Text>
+              <Text>{item.date.when}</Text>
+              <View style={styles.imageContainer}></View>
+              <Text
+                style={{ color: "blue" }}
+                onPress={() => {
+                  Linking.openURL(item.link);
+                }}
+              >
+                More About This Event
+              </Text>
+            </View>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={{ color: "white" }}>close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </TouchableOpacity>
+  );
+};
+
+export default EventsCard;
+
+const styles = StyleSheet.create({
+  item: {
+    textAlign: "center",
+    backgroundColor: "#84A98C",
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#CAD2C5",
+    padding: 50,
+    fontSize: 20,
+    margin: 5,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "#faf9f6",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    padding: 10,
+    backgroundColor: "#6667AB",
+  },
+});
--- components/SearchTabs/SearchEvents.jsx
+++ components/SearchTabs/SearchEvents.jsx
@@ -10,7 +10,7 @@ import {
   Modal,
   Pressable,
 } from "react-native";
-import { fetchEvents } from "../../fetch-api/events";
+import EventsCard from "./EventsCard";
 
 const SearchEvent = () => {
   const [events, setEvents] = useState([]);
@@ -31,75 +31,7 @@ const SearchEvent = () => {
     <View style={styles.listContainer}>
       <FlatList
         data={events}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={() => {
-              setModalVisible(true);
-            }}
-          >
-            <View style={styles.item}>
-              <Text>Title: {item.title}</Text>
-              <Image
-                style={{ height: 100, width: 100, borderRadius: 50 }}
-                source={{ uri: item.image }}
-              />
-              <Text>
-                Address: {item.address[0]}, {item.address[1]}
-              </Text>
-              <Text>{item.date.when}</Text>
-              <View style={styles.imageContainer}></View>
-              <Text
-                style={{ color: "blue" }}
-                onPress={() => {
-                  Linking.openURL(item.link);
-                }}
-              >
-                More About This Event
-              </Text>
-            </View>
-            <Modal
-              animationType="slide"
-              transparent={true}
-              visible={modalVisible}
-              onRequestClose={() => {
-                setModalVisible(!modalVisible);
-              }}
-            >
-              <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-                  <View>
-                    <Text>Title: {item.title}</Text>
-                    <Image
-                      style={{ height: 100, width: 100, borderRadius: 50 }}
-                      source={{ uri: events.image }}
-                    />
-                    <Text>
-                      Address: {item.address[0]}, {item.address[1]}
-                    </Text>
-                    <Text>{item.date.when}</Text>
-                    <View style={styles.imageContainer}></View>
-                    <Text
-                      style={{ color: "blue" }}
-                      onPress={() => {
-                        Linking.openURL(item.link);
-                      }}
-                    >
-                      More About This Event
-                    </Text>
-                  </View>
-                  <Pressable
-                    style={[styles.button, styles.buttonClose]}
-                    onPress={() => {
-                      setModalVisible(!modalVisible);
-                    }}
-                  >
-                    <Text style={{ color: "white" }}>close</Text>
-                  </Pressable>
-                </View>
-              </View>
-            </Modal>
-          </TouchableOpacity>
-        )}
+        renderItem={({ item }) => <EventsCard item={item} />}
       />
     </View>
   );
@@ -108,46 +40,6 @@ const SearchEvent = () => {
 const styles = StyleSheet.create({
   listContainer: {
     backgroundColor: "#52796F",
-  },
-  item: {
-    textAlign: "center",
-    backgroundColor: "#84A98C",
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: "#CAD2C5",
-    padding: 50,
-    fontSize: 20,
-    margin: 5,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "#faf9f6",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonClose: {
-    padding: 10,
-    backgroundColor: "#6667AB",
   },
 });
 
