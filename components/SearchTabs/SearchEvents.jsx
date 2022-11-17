8236ba5611ec63bd73d224f33fd388e57071955c
GxrySu <garysu2013@gmail.com>
2022-11-17 12:32:32 +0000

    added events api and attempt to add pop up for each events card

--- components/SearchTabs/SearchEvents.jsx
+++ components/SearchTabs/SearchEvents.jsx
@@ -1,23 +1,105 @@
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Linking,
+  Image,
+  Modal,
+  Pressable,
+} from "react-native";
+import { fetchEvents } from "../../fetch-api/events";
 
 const SearchEvent = () => {
-  const testData = [
-    { title: "Event1" },
-    { title: "Event2" },
-    { title: "Event3" },
-    { title: "Event4" },
-    { title: "Event5" },
-    { title: "Event6" },
-    { title: "Event7" },
-    { title: "Event8" },
-    { title: "Event9" },
-  ];
+  const [events, setEvents] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      "https://serpapi.com/search.json?engine=google_events&q=Events+in+United+Kingdom&api_key=cbf9cea56cfe76bcdad1f5f2a29f887f54dafebcc8d26e33f0ca2392fa1860ed&start=100"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents(data.events_results);
+      });
+  }, []);
 
   return (
     <View style={styles.listContainer}>
       <FlatList
-        data={testData}
-        renderItem={({ item }) => <TouchableOpacity><Text style={styles.item}>{item.title}</Text></TouchableOpacity>}
+        data={events}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => {
+              setModalVisible(true);
+            }}
+          >
+            <View style={styles.item}>
+              <Text>Title: {item.title}</Text>
+              <Image
+                style={{ height: 100, width: 100, borderRadius: 50 }}
+                source={{ uri: item.image }}
+              />
+              <Text>
+                Address: {item.address[0]}, {item.address[1]}
+              </Text>
+              <Text>{item.date.when}</Text>
+              <View style={styles.imageContainer}></View>
+              <Text
+                style={{ color: "blue" }}
+                onPress={() => {
+                  Linking.openURL(item.link);
+                }}
+              >
+                More About This Event
+              </Text>
+            </View>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <View>
+                    <Text>Title: {item.title}</Text>
+                    <Image
+                      style={{ height: 100, width: 100, borderRadius: 50 }}
+                      source={{ uri: events.image }}
+                    />
+                    <Text>
+                      Address: {item.address[0]}, {item.address[1]}
+                    </Text>
+                    <Text>{item.date.when}</Text>
+                    <View style={styles.imageContainer}></View>
+                    <Text
+                      style={{ color: "blue" }}
+                      onPress={() => {
+                        Linking.openURL(item.link);
+                      }}
+                    >
+                      More About This Event
+                    </Text>
+                  </View>
+                  <Pressable
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => {
+                      setModalVisible(!modalVisible);
+                    }}
+                  >
+                    <Text style={{ color: "white" }}>close</Text>
+                  </Pressable>
+                </View>
+              </View>
+            </Modal>
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
@@ -33,9 +115,39 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 2,
     borderColor: "#CAD2C5",
-    padding: 100,
+    padding: 50,
     fontSize: 20,
-    margin: 5
+    margin: 5,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "#faf9f6",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    padding: 10,
+    backgroundColor: "#6667AB",
   },
 });
 
