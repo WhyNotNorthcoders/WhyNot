9039e92a4a2fb30d3000614d56da9bfd76e1062b
GxrySu <garysu2013@gmail.com>
2022-11-18 10:37:26 +0000

    add custom profile picture

--- components/Profiles/ProfileDetails.jsx
+++ components/Profiles/ProfileDetails.jsx
@@ -1,11 +1,49 @@
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  View,
+  Pressable,
+  Image,
+} from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import * as ImagePicker from "expo-image-picker";
 
 const ProfileDetails = () => {
+  const [image, setImage] = useState("");
+  const [imageSelected, setImageSelected] = useState(false);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      setImageSelected(true);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.profileDetails}>
       <Text style={styles.username}>Nabeel</Text>
-      <Ionicons name={"person-outline"} size={40} style={styles.ionicons} />
+
+      {imageSelected ? (
+        <Pressable onPress={pickImage}>
+          <View>
+            {image && <Image source={{ uri: image }} style={styles.ionicons} />}
+          </View>
+        </Pressable>
+      ) : (
+        <Pressable onPress={pickImage}>
+          <Ionicons name={"person-outline"} style={styles.ionicons} />
+        </Pressable>
+      )}
     </SafeAreaView>
   );
 };
@@ -28,7 +66,7 @@ const styles = StyleSheet.create({
     width: 80,
     height: 80,
     borderWidth: 2,
-    marginTop: -20,
+    marginTop: -25,
     borderColor: "white",
     marginLeft: 20,
   },
